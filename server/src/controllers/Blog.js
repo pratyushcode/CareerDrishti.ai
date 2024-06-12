db66481da0698412309d4468bcd923aeb772083d
pratyushcode <pratyushksinha21@gmail.com>
2024-06-12 23:05:32 +0530

    backend

---
+++ server/src/controllers/Blog.js
@@ -0,0 +1,19 @@
+const Blog=require("../models/Blog");
+
+
+async function GetBlog(req,res)
+{
+    let blog;
+    try { 
+        blog =await Blog.find()
+    } catch (error) {
+        console.log(blog);
+    }
+
+    return res.json({blog});
+
+}
+
+module.exports={
+    GetBlog
+}
